3cf887cf2424ba192f121b5afd3395ff6b3bb260
taise <taise515@gmail.com>
2013-12-07 14:57:06 +0900

    UserTest load users.yaml data

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -2,20 +2,34 @@ package models;
 
 import models.User;
 import org.junit.*;
+import java.util.List;
+import play.libs.Yaml;
+import com.avaje.ebean.Ebean;
 
-import static org.fest.assertions.Assertions.*;
+import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
 
 public class UserTest {
-    @Test
-    public void userFindByEmail() {
-        String email = "alice@email.com";
+    static final String EMAIL = "alice@email.com";
+
+    @Before
+    public void setUp() {
         start(fakeApplication(inMemoryDatabase()));
-        User user = new User("alice", email, "password");
-        user.save();
+        Ebean.save((List) Yaml.load("testData/users.yml"));
+    }
+    
+    @Test
+    public void findByEmail() {
+        User actual = User.findByEmail(EMAIL);
 
-        User actual = User.findByEmail(email);
+        assertThat(actual.email).isEqualTo(EMAIL);
+    }
 
-        assertThat(actual.email).isEqualTo(email);
+    @Test
+    public void dontFindByInvalidEmail() {
+        String typoEmail = EMAIL + ".";
+        User actual = User.findByEmail(typoEmail);
+
+        assertThat(actual).isNull();
     }
 }
