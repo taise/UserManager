ded4b3ba2446c956eb107ed89f156095e71a55bd
taise <taise515@gmail.com>
2013-12-08 23:30:49 +0900

    Root request redirect to UserController.index

---
+++ test/RouteTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import play.mvc.Result;
+
+import static org.fest.assertions.Assertions.*;
+import static play.test.Helpers.*;
+
+public class RouteTest {
+    @Test public void requestRootGet() {
+       start(fakeApplication());
+
+       Result result = route(fakeRequest(GET, "/"));
+       assertThat(header("Location", result)).isEqualTo("/users");
+    }
+}
