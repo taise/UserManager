91aacfa01b97c23a74ef29eb7e6fbc68e961cff0
taise <taise515@gmail.com>
2013-12-15 15:56:59 +0900

    Create ApiController.allUsers & test

---
+++ app/controllers/ApiController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+import models.User;
+
+public class ApiController extends Controller {
+
+    @BodyParser.Of(play.mvc.BodyParser.Json.class)
+    public static Result allUsers() {
+        List<User> users = User.find.findList();
+        JsonNode json = Json.toJson(users);
+        return ok(json);
+    }
+}
---
+++ test/controllers/ApiControllerTest.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import org.junit.*;
+import play.libs.Yaml;
+import java.util.*;
+import com.avaje.ebean.*;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import static org.fest.assertions.api.Assertions.*;
+import static play.test.Helpers.*;
+
+import play.mvc.*;
+import models.User;
+
+public class ApiControllerTest {
+    @Before public void setUp() {
+        start(fakeApplication(inMemoryDatabase()));
+    }
+
+    @BodyParser.Of(BodyParser.Json.class)
+    @Test public void callAllUser() {
+        Ebean.save((List) Yaml.load("testData/users.yml"));
+
+        Result result = callAction(
+                controllers.routes.ref.ApiController.allUsers()
+                );
+
+        String content = contentAsString(result);
+        assertThat(status(result)).isEqualTo(OK);
+        assertThat(contentType(result)).isEqualTo("application/json");
+        assertThat(content).contains("[{\"id\":1");
+        assertThat(content).contains("},{\"id\":2");
+    }
+}
